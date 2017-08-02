5d469ce23da5bc226f763eeaa9da39c16acae40b
¨Mario <mario.vizcainos@gmail.com>
2017-08-02 09:29:08 -0500

    added messages to the validations

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,10 +13,20 @@
 #
 
 class Room < ActiveRecord::Base
-  validates :title, presence: true
-  validates :description, presence: true, maximum:  400
-  validates :beds, presence: true
-  validates :guests, presence: true
-  validates :beds,:guest, format: { with: /\A\d+\z/, message: "Integer only. No sign allowed." }
-  validates :image_url, presence: true
+  validates :title, presence: true,
+    message: "El titlo no puede estar en blanco :^("
+  
+    validates :description, presence: true, maximum:  400,
+    message: "La descripcion es obligatoria y maximo 400 caracteres!!!!!"
+  
+    validates :beds, presence: true,
+    message: "Este campo es obligatorio mi amigo"
+  
+    validates :guests, presence: true,
+    message: "Etse opmac se oirotagilbo im ogima"
+  
+  validates :beds,:guest, format: { with: /\A\d+\z/, message: "Solo numeros!!" }
+  
+    validates :image_url, presence: true,
+    message: "Santas bativalidaciones tenemos problemas! este campo es obligatorio"
 end
