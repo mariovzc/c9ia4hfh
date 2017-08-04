29900d628729d76c3012e747426ffbf35f0b771c
¨Mario <mario.vizcainos@gmail.com>
2017-08-04 16:48:17 -0500

    added edit methods

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,13 @@
 class RoomsController < ApplicationController
+  before_action :set_room, only: [:edit, :update, :destroy]
   def index
     @rooms = Room.all
   end
 
   def new
     @room = Room.new
+  end
+  def edit
   end
 
   def create
@@ -15,12 +18,21 @@ class RoomsController < ApplicationController
       render :new
     end
   end
+  def update
+   if @room.update(room_params)
+     redirect_to rooms_path
+   else
+     render :edit
+   end
+  end
   def destroy
-    @room = Room.find(params[:id])
     @room.destroy
     redirect_to rooms_path
   end
   protected
+    def set_room
+      @room = Room.find(params[:id]);
+    end
     def room_params
       params.require(:room).permit(:title, :description, :beds, :guests, :image_url, :price_per_night)
     end
